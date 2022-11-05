e23ebc7a4e6bc8ac4c85141dd9a3018b69c8135a
trv893 <trevorhilimire@gmail.com>
2022-11-05 11:43:07 -0400

    Add project files.

---
+++ MauiApp3/Data/UserTag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp3.Data
+{
+    public class UserTag
+    {
+        public int Id { get; set; }
+        public string Tag { get; set; }
+        public int NumberOfTimesUsed{ get; set; }
+
+    }
+}
